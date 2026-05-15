fb5d9fdd2ca11183435efc9f669cbb0feb621107
paul lulami <lula.03paul@tutamail.com>
2026-05-15 13:49:55 +0300

    fix: add access_config to EKS cluster (API_AND_CONFIG_MAP auth mode)
    Cluster was created with default CONFIG_MAP mode which blocks Access Entry
    API calls. Setting API_AND_CONFIG_MAP enables both the legacy aws-auth
    ConfigMap and the new EKS Access Entry API simultaneously.

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -36,6 +36,11 @@ resource "aws_eks_cluster" "this" {
     endpoint_private_access = false
   }
 
+  access_config {
+    authentication_mode                         = "API_AND_CONFIG_MAP"
+    bootstrap_cluster_creator_admin_permissions = true
+  }
+
   depends_on = [
     aws_iam_role_policy_attachment.cluster_policy
   ]
